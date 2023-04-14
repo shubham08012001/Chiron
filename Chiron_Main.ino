abf877a4695c9b1bad1d7475f4442a2b29e08c69
Sarvesh Lad <55616027+Crizac@users.noreply.github.com>
2023-04-14 17:35:35 +0530

    Chiron Main - Calibrated
    All hardware issues are debugged and the code is updated for newly calibrated sensors

--- Chiron_Main.ino
+++ Chiron_Main.ino
@@ -1,19 +1,14 @@
-// Define pins for the alcohol sensor and switches
-/* Replace these values with your own readings */
-
 #define Relay 5
-#define Sober 299   // Define max value that we consider sober
+#define Sober 150   // Define max value that we consider sober
 #define Drunk 300   // Define min value that we consider drunk
-#define MQ3pin A0
+#define MQ3pin 7
 float sensorValue;  //variable to store sensor value
-const int switch1Pin = 2;
-const int switch2Pin = 3;
-#define LtrigPin1 11// Define pins for the ultrasonic sensors
+const int switchPin = 3;
+#define LtrigPin1 11 // Define pins for the ultrasonic sensors
 #define LechoPin1 12
 #define RtrigPin2 9
 #define RechoPin2 10
-long duration, distance, RightSensor,LeftSensor;// Define variables for the ultrasonic sensors
-// Define variables for the alcohol sensor
+long duration, distance, RightSensor,LeftSensor; // Define variables for the ultrasonic sensors
 
 
 void setup()
@@ -24,24 +19,25 @@ pinMode(LtrigPin1, OUTPUT);
 pinMode(LechoPin1, INPUT);
 pinMode(RtrigPin2, OUTPUT);
 pinMode(RechoPin2, INPUT);
+pinMode(MQ3pin, INPUT);
 
-// Set the switch pins as inputs
-pinMode(switch1Pin, INPUT_PULLUP);
-pinMode(switch2Pin, INPUT_PULLUP);
+pinMode(switchPin, INPUT);
 
 pinMode(Relay, OUTPUT);
+
+Serial.println("Chiron is now Active");
 }
 
 void loop() {
-  sensorValue = analogRead(MQ3pin); 
-  // Check if both switches are pressed and alcohol is not detected
-  if (digitalRead(switch1Pin) == LOW && digitalRead(switch2Pin) == LOW && sensorValue < 300) 
+  
+  sensorValue = digitalRead(MQ3pin); 
+  
+  if (digitalRead(switchPin) == HIGH && sensorValue == HIGH) // Check if switches are pressed and alcohol is not detected
    {
-      if (sensorValue >= Sober && sensorValue < Drunk)
-   {
+     Serial.println("Helmet is Worn");
     
-     Serial.println("  |  Status: Bike starts since rider is SOBER");
-
+     Serial.println("Status: Bike starts since rider is SOBER");
+    
      digitalWrite(Relay,HIGH);
      
      SonarSensor(LtrigPin1, LechoPin1);
@@ -49,18 +45,29 @@ void loop() {
      SonarSensor(RtrigPin2, RechoPin2);
      RightSensor = distance;
 
+     Serial.print("Left Distance - ");
      Serial.print(LeftSensor);
-     Serial.print(" - ");
+     Serial.print(" | ");
+     Serial.print("Right Distance - ");
      Serial.println(RightSensor);
-     Serial.print(" - ");
 
-   }
-   else 
+     delay (500);
+    }
+    
+    else if (digitalRead(switchPin) == HIGH && sensorValue == LOW)
+    
+    {
+     Serial.println("Status: Bike wont start since rider is DRUNK");
+     delay (500);
+    }
+
+  else 
+  
   {
-     Serial.println("  |  Status: Bike wont start since rider is DRUNK");
+    Serial.println("Sensors not Detected");
+    delay(500);
   }
-
-  }
+  
 }
 
 void SonarSensor(int trigPin,int echoPin)
